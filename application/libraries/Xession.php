9f503dc208a6a892b2aac01bed2e671f0230acb2
Black Divine <munir.ehmad@gmail.com>
2011-08-21 10:47:21 +0500

    Fixed some minor bugs with setflash() Catered case where user can accidently overwrite flashdata

--- application/libraries/Xession.php
+++ application/libraries/Xession.php
@@ -1,33 +1,34 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 /**
- * CodeIgniter Alternate Session Class because default one Sucks!
+ * CodeIgniter Alternate Session Class (utilizing PHP's native session) because the default one Sucks!
  *
- * @package        	CodeIgniter
- * @subpackage    	Libraries
- * @category    		Libraries
- * @author        	Munir Ahmad	
- * @created					08/15/2011
- * @link						http://github.com/munirehmad/Xession/
+ * @package         CodeIgniter
+ * @subpackage      Libraries
+ * @category        Libraries
+ * @author          Munir Ahmad	
+ * @created         04/06/2009
+ * @link						http://github.com/munirehmad/Xession
  */
 
 class Xession
 {
 	protected $_CI;
+	protected $_FLASHDATA_KEY = '_f_d_'; 	// must be pretty unique to avoid collision with setdata()
 	
 	function __construct( $config = array() )
 	{
-		$this->_CI =& get_instance();
+    $this->_CI =& get_instance();
 
 		session_start();
 
 
 		// flash data chores
-		if ( isset($_SESSION['f']) )
+		if ( isset($_SESSION[$this->_FLASHDATA_KEY]) )
 		{
-			foreach ( $_SESSION['f'] as $k => &$v )
+			foreach ( $_SESSION[$this->_FLASHDATA_KEY] as $k => &$v )
 			{
 				if ( $v['n'] < 1 )
-					unset($_SESSION['f'][$k]);
+					unset($_SESSION[$this->_FLASHDATA_KEY][$k]);
 				else
 					$v['n']--;
 			}
@@ -36,7 +37,7 @@ class Xession
 		//var_dump($_SESSION);
 	}
 	
-	function set( $key, $val )
+	function set( $key, $val = '' )
 	{
 		if ( is_array($key) )
 		{
@@ -49,18 +50,25 @@ class Xession
 		$_SESSION[$key] = $val;
 	}
 
-	function setflash( $data = array() )
+	function setflash( $key, $val = '' )
 	{
-		foreach( $data as $k => $val )
+		if ( is_array($key) )
 		{
-			//echo '<p>Got '.$k.' => '.$val.'</p>';
-			$_SESSION['f'][$k] = array( 'd' => $val, 'n' => 1 );
+			foreach( $key as $k => $val )
+			{
+				//echo '<p>Got '.$k.' => '.$val.'</p>';
+				$_SESSION[$this->_FLASHDATA_KEY][$k] = array( 'd' => $val, 'n' => 1 );
+			}
+
+			return;
 		}
+
+		$_SESSION[$this->_FLASHDATA_KEY][$key] = array( 'd' => $val, 'n' => 1);
 	}
 
 	function getflash( $key )
 	{
-		return isset($_SESSION['f'][$key]) ? $_SESSION['f'][$key]['d'] : NULL;
+		return isset($_SESSION[$this->_FLASHDATA_KEY][$key]) ? $_SESSION[$this->_FLASHDATA_KEY][$key]['d'] : NULL;
 	}
 
 	function get($key)
